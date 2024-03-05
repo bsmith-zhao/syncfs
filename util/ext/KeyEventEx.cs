c3099cb5ea50829ec02437de80d008dbaac2efbf
bsmith-zhao <143573542+bsmith-zhao@users.noreply.github.com>
2024-03-05 12:25:23 +0800

    update xtext

---
+++ util/ext/KeyEventEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace util.ext
+{
+    public static class KeyEventEx
+    {
+        public static bool CtrlV(this KeyEventArgs e)
+            => e.Control && e.KeyCode == Keys.V;
+
+        public static bool CtrlS(this KeyEventArgs e)
+            => e.Control && e.KeyCode == Keys.S;
+    }
+}
